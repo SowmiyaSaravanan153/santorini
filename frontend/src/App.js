497c4a3570235e12a1a4d00518b78422a06a5e4f
Sowmiya Saravanan <140613799+SowmiyaSaravanan153@users.noreply.github.com>
2025-08-04 05:47:59 +0000

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,10 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <SantoriniHome />
       <Contactdetails />
       <ContactSection /> 
-      <SantoriniHome />
+      
     </div>
   );
 }
